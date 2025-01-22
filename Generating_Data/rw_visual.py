b6a7e180dd9e424421ec7463384c1fe23e83d02b
MelvinKhakabo <melvinkhakabo@gmail.com>
2025-01-22 12:16:32 +0300

    Data Generation for Random Walk Project

--- Generating_Data/rw_visual.py
+++ Generating_Data/rw_visual.py
@@ -12,8 +12,6 @@ plt.show()
 
 
 #Generating Multiple random walks
-import matplotlib.pyplot as plt
-from random_walk import RandomWalk
 
 # Keep making new walks, as long as the program is active.
 while True:
@@ -30,5 +28,29 @@ while True:
     # Ask user if they want to make another walk
     keep_running = input("Make another walk? (y/n): ")
     
+    if keep_running == 'n':
+        break
+
+
+# Styling: Coloring the points
+
+# Keep making new walks, as long as the program is active.
+while True:
+    # Make a random walk, and plot the points.
+    rw = RandomWalk()
+    rw.fill_walk()
+    
+    # Generate point numbers for coloring
+    point_numbers = list(range(rw.num_points))
+    
+    # Plot the random walk points with color mapping
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
+                edgecolor='none', s=15)
+    
+    # Display the plot
+    plt.show()
+
+    # Ask if the user wants to make another walk
+    keep_running = input("Make another walk? (y/n): ")
     if keep_running == 'n':
         break
