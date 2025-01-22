f26e9e928751cf6e61b568ef56f09b119cb32017
MelvinKhakabo <melvinkhakabo@gmail.com>
2025-01-22 08:53:35 +0300

    Data Generation for Randowm Walk project

--- Generating_Data/rw_visual.py
+++ Generating_Data/rw_visual.py
@@ -1,6 +1,11 @@
 # Plotting the Random Walk
 import matplotlib.pyplot as plt
 from random_walk import RandomWalk
+
 # Make a random walk, and plot the points.
 rw = RandomWalk()
-rw.fill_walk()
+rw.fill_walk()  # This will now work
+
+# Plotting the points
+plt.plot(rw.x_values, rw.y_values, linewidth=1)
+plt.show()
